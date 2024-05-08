5d2e0253f14a7caeb000e87173016ebe082dcbc5
DinoJw <162787283+maesung-git-company@users.noreply.github.com>
2024-05-08 21:57:33 +0900

    supabase connecting test

--- project/2511/contract/lib/main.dart
+++ project/2511/contract/lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: 'assets/config/.env');
+
+  await Supabase.initialize(
+    url: dotenv.get('SUPABASE_URL'),
+    anonKey: dotenv.get('SUPABASE_KEY'),
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,26 +19,46 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("asd"),
-        ),
-        body: Container(
-          
-        ),
-        bottomNavigationBar: BottomAppBar(),
-      )
+    return const MaterialApp(
+      title: 'Countries',
+      home: HomePage(),
     );
   }
 }
 
-class TopBarContainer extends StatelessWidget {
-  const TopBarContainer({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _future = Supabase.instance.client
+      .from('countries')
+      .select();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: FutureBuilder(
+        future: _future,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final countries = snapshot.data!;
+          return ListView.builder(
+            itemCount: countries.length,
+            itemBuilder: ((context, index) {
+              final country = countries[index];
+              return ListTile(
+                title: Text(country['name']),
+              );
+            }),
+          );
+        },
+      ),
+    );
   }
 }
